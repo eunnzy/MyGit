49d36c1885eb04c66829fd18474db71ef819d417
eunz12 <silveryeunji@gmail.com>
2022-06-18 02:14:46 +0900

    Add line after branching

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -3,6 +3,8 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Hello This is A class");
+		System.out.println("After branching");
+		System.out.println("Add line");
 	}
 
 }
