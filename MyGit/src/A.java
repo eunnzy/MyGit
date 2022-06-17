321bf90ff4d57e50097e931994b4ad540bb6c4aa
eunz12 <silveryeunji@gmail.com>
2022-06-18 02:11:56 +0900

    Create devB branch

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -3,6 +3,9 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Hello This is A class");
+		
+		B bTest = new B();
+		bTest.devB();
 	}
 
 }
--- MyGit/src/B.java
+++ MyGit/src/B.java
@@ -3,4 +3,9 @@ public class B {
 	public void printB() {
 		System.out.println("Hello This is B class");
 	}
+	
+	public void devB() {
+		System.out.println("This is branching");
+		System.out.println("It's devB");
+	}
 }
